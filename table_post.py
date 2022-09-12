d3f7faece87a03f9e9f419b0a7fc0f3c7c4e659d
George Novozhilov <gnovozhiloff@gmail.com>
2022-09-12 11:30:57 +0300

    add class User

--- table_post.py
+++ table_post.py
@@ -9,6 +9,18 @@ class Post(Base):
     topic = Column(String)
 
 
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    gender = Column(Integer)
+    age = Column(Integer)
+    country = Column(String)
+    city = Column(String)
+    exp_group = Column(Integer)
+    os = Column(String)
+    source = Column(String)
+
+
 if __name__ == '__main__':
     session = SessionLocal()
     li = []
